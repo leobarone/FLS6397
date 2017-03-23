33f25fbbfbff4884257ecf0bf63d33d01d520e67
leobarone <leobarone@gmail.com>
2017-03-23 16:23:51 -0300

    3a data

--- miscelanea/import_exemplos_bf.R
+++ miscelanea/import_exemplos_bf.R
@@ -2,7 +2,7 @@ rm(list=ls())
 library(dplyr)
 mydb <- src_mysql(dbname = "BF",
                      user = "root",
-                     password = "")
+                     password = "Dradu#2013")
 
 src_tbls(mydb)
 bf <- tbl(mydb, "BF201608")
@@ -16,7 +16,7 @@ bf <- bf %>%
   select(uf, codmunic, munic, nis, valor) %>%
   collect()
 
-bf <- bf %>% sample_n(100)
+bf <- bf %>% sample_n(10)
 
 
 pasta <- "/home/acsa/FLS6397/data"
@@ -28,6 +28,9 @@ write.table(bf, file.path(pasta, "bf_amostra_hp.csv"),
 
 write.table(bf, file.path(pasta, "bf_amostra_hv.csv"), 
             sep = ",", col.names = T, row.names = F)
+
+write.table(bf, file.path(pasta, "bf_amostra_np.csv"), 
+            sep = ",", col.names = F, row.names = F)
 
 write.table(bf, file.path(pasta, "bf_amostra_np.csv"), 
             sep = ";", col.names = F, row.names = F)
