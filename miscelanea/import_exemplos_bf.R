233d2a9084e23c5bd5f5cdb1d86ea1e53f39b80e
leobarone <leobarone@gmail.com>
2017-03-23 15:59:23 -0300

    3a aula 1st commit

---
+++ miscelanea/import_exemplos_bf.R
@@ -0,0 +1,33 @@
+rm(list=ls())
+library(dplyr)
+mydb <- src_mysql(dbname = "BF",
+                     user = "root",
+                     password = "")
+
+src_tbls(mydb)
+bf <- tbl(mydb, "BF201608")
+print(bf$ops$vars)
+head(bf)
+
+bf <- bf %>%
+  filter(Codigo_Funcao != 0) %>%
+  rename(uf = UF, codmunic = Codigo_SIAFI_Municipio, 
+         munic = Nome_Municipio, nis = NIS_Favorecido, valor = Valor_Parcela) %>%
+  select(uf, codmunic, munic, nis, valor) %>%
+  collect()
+
+bf <- bf %>% sample_n(100)
+
+
+pasta <- "/home/acsa/FLS6397/data"
+write.table(bf, file.path(pasta, "bf_amostra_ht.csv"), 
+                          sep = "/t", col.names = T, row.names = F)
+
+write.table(bf, file.path(pasta, "bf_amostra_hp.csv"), 
+            sep = ";", col.names = T, row.names = F)
+
+write.table(bf, file.path(pasta, "bf_amostra_hv.csv"), 
+            sep = ",", col.names = T, row.names = F)
+
+write.table(bf, file.path(pasta, "bf_amostra_np.csv"), 
+            sep = ";", col.names = F, row.names = F)
